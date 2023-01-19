dcb655010f0f33150ff5a725d537160c8b43303d
Thomas Wood <woodthom@gmail.com>
2023-01-19 15:12:27 +0000

    Fix deployment script

--- front_end/build_deploy.sh
+++ front_end/build_deploy.sh
@@ -1,4 +1,4 @@
-export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0100_/_/g`
+export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g`
 docker build -t protocols-front --build-arg COMMIT_ID=$COMMIT_ID .
 docker tag protocols-front regprotocolsfds.azurecr.io/protocols-front:$COMMIT_ID
 docker push regprotocolsfds.azurecr.io/protocols-front:$COMMIT_ID
