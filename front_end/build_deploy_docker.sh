59c9256961ba7f86b5b26b538d4dd3fee1483aff
Thomas Wood <thomas@fastdatascience.com>
2023-06-23 21:48:07 +0000

    keep version no

--- front_end/build_deploy_docker.sh
+++ front_end/build_deploy_docker.sh
@@ -1,5 +1,5 @@
 docker login -u fastdatascience
 export COMMIT_ID=`git show -s --format=%ci_%h | sed s/[^_a-z0-9]//g | sed s/0[012]00_/_/g`
 docker build -t protocols-front --build-arg COMMIT_ID=$COMMIT_ID .
-docker tag protocols-front fastdatascience/clinical_trial_risk:latest
-docker push fastdatascience/clinical_trial_risk:latest
+docker tag protocols-front fastdatascience/clinical_trial_risk:$COMMIT_ID
+docker push fastdatascience/clinical_trial_risk:$COMMIT_ID
